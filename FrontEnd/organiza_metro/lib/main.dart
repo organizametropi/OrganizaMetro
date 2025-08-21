7bb32113b4795b3d73a217b8a62fe7935a3d02e8
GabrielFOP <gabrielferoliver082@gmail.com>
2025-08-21 18:19:11 -0300

    Init Flutter and set Databases Connections

---
+++ FrontEnd/organiza_metro/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue[700],
+        body: Center(
+          child: Container(
+            height: 300, 
+            width: 300, 
+            color: Colors.white,
+            padding: EdgeInsets.all(25),
+            child: Text("TEST"),
+            ),
+        ),
+      ),
+    );
+  }
+}
